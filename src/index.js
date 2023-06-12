b3f35d00036145d37ce196813b64dbfa6f8d76ac
ALAA <111809556+AlaaEddineSidqui@users.noreply.github.com>
2023-06-12 08:50:31 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Employe from './Employe';
+import Employes from './Employes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Employe />
+    <Employes />
   </React.StrictMode>
 );
 
